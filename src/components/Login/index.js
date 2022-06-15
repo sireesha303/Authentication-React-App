2736715ed33efa0ebbef1829369069ffa8f5a6e3
sireesha <msireesha30031999@gmail.com>
2022-06-15 20:05:18 +0530

    Cookie Implementaiton -> storing jwt token in cookie and from cookie handling login and home page related routing

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect,useState } from 'react';
+import { useNavigate} from 'react-router-dom';
+import Cookies from 'js-cookie';
+
 
 import './index.css'
 
 const Login = () =>{
     const [userName,setUserName] =useState();
     const [passWord,setPassWord] = useState();
-    const [loginError,setLoginError] = useState({showLoginError:false,
-    loginErrorMsg:""
-    });
-
-
+    const [loginError,setLoginError] = useState({showLoginError:false,loginErrorMsg:""});
+    const [isLoogedIn,setLoginStatus] = useState(false);
 
     const navigate = useNavigate();
+
 
     const onChangeOfUserName = (event) => {
         // console.log(event.target.value);
@@ -24,7 +24,8 @@ const Login = () =>{
         setPassWord(event.target.value)
     }
 
-    const onLoginSuccess = () =>{
+    const onLoginSuccess = jwtToken =>{
+        Cookies.set('jwt_token',jwtToken,{expires:2})
         navigate("/");
     }
 
@@ -44,11 +45,11 @@ const Login = () =>{
             body: JSON.stringify({ username:userName,password:passWord})
         };
         const response = await fetch(url, requestOptions);
-        console.log(response);
+        // console.log(response);
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         if(response.ok === true){
-            onLoginSuccess()
+            onLoginSuccess(data.jwt_token)
         }
         else{
             onLoginFailure(data.error_msg)
@@ -88,31 +89,46 @@ const Login = () =>{
             </>
         )
     }
+
+     useEffect(()=>{
+         const jwtTokenFromCookie = Cookies.get('jwt_token');
+         if(jwtTokenFromCookie !== undefined){
+             setLoginStatus(true);
+         }  
+     },[]);
     
-    return(
-        <div className="login-page-container">
-            <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
-            alt="Website Logo"
-            className="logo-mobile-view-img"
-            />
-            <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png" 
-            alt="Login Img"
-            className='login-image'
-            />
-            <form className='form-container' onSubmit={submitLoginForm}>
+    switch(isLoogedIn){
+        case true:
+            return navigate("/");
+            
+        case false:
+            return(
+                <div className="login-page-container">
                 <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
-                alt="Website logo"
-                className='logo-desktop-view-img'
+                alt="Website Logo"
+                className="logo-mobile-view-img"
                 />
-                <div className='input-container'>{renderUserNameField()}</div>
-                <div className='input-container'>{renderPassWordField()}</div>
-                <button className='login-btn' type="submit">
-                    Login
-                </button>
-                {loginError.showLoginError && <p className='login-error-msg'>* {loginError.loginErrorMsg}</p>}
-            </form>
-        </div>
-    )
+                <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png" 
+                alt="Login Img"
+                className='login-image'
+                />
+                <form className='form-container' onSubmit={submitLoginForm}>
+                    <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+                    alt="Website logo"
+                    className='logo-desktop-view-img'
+                    />
+                    <div className='input-container'>{renderUserNameField()}</div>
+                    <div className='input-container'>{renderPassWordField()}</div>
+                    <button className='login-btn' type="submit">
+                        Login
+                    </button>
+                    {loginError.showLoginError && <p className='login-error-msg'>* {loginError.loginErrorMsg}</p>}
+                </form>
+            </div>
+            )
+        default:
+            return null;
+    }   
 }
 
 export default Login
